7a7a1b9379b3c5840fc3c9b76148cbbedb2b0961
Pormiiles <andrelima.santos2370@gmail.com>
2024-07-14 15:06:42 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CalculadoraIMC());
+
+class CalculadoraIMC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TelaPrincipal(),
+    );
+  }
+}
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CALCULADORA IMC'),
+      ),
+      body: Center(
+        child: Text('Corpo do texto'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
