bfa7e799ff6a83ff392aa5060e6a76f2c61ad705
Pormiiles <andrelima.santos2370@gmail.com>
2024-07-18 01:02:37 -0300

    Criando base para widget de gênero do user

--- lib/tela_principal.dart
+++ lib/tela_principal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TelaPrincipal extends StatefulWidget {
   @override
@@ -14,43 +15,89 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
       ),
       body: Column(
         children: [
-          Expanded(child: Row(
-            children: [
-              Expanded(child: 
-                CartaoPadrao(Color(0xFF9E9E9E)),
-              ),
-              Expanded(child: 
-                CartaoPadrao(Color(0xFF9E9E9E)),
-              )
-            ],
-          ),),
-          Expanded(child: Row(
-            children: [
-              Expanded(child:
-                CartaoPadrao(Color(0xFF9E9E9E)),
-              )
-            ],
-          )),
-          Expanded(child: Row(
-            children: [
-              Expanded(child:
-                CartaoPadrao(Color(0xFF9E9E9E)),
-              ),
-              Expanded(child:
-                CartaoPadrao(Color(0xFF9E9E9E)),
-              )
-            ],
-          )),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CartaoPadrao(
+                    Color(0xFF9E9E9E),
+                    filhoCartao: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 95.0,
+                        ),
+                        SizedBox(height: 15.0),
+                        Text(
+                          'MASCULINO',
+                          style: TextStyle(color: Colors.black,
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: CartaoPadrao(
+                    Color(0xFF9E9E9E),
+                    filhoCartao: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.venus,
+                          size: 95.0,
+                        ),
+                        SizedBox(height: 15.0),
+                        Text(
+                          'FEMININO',
+                          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CartaoPadrao(
+                    Color(0xFF9E9E9E),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CartaoPadrao(
+                    Color(0xFF9E9E9E),
+                  ),
+                ),
+                Expanded(
+                  child: CartaoPadrao(
+                    Color(0xFF9E9E9E),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
 
 class CartaoPadrao extends StatelessWidget {
-  CartaoPadrao(this.cor);
+  CartaoPadrao(this.cor, {this.filhoCartao});
 
   final Color cor;
+  final Widget? filhoCartao;
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +107,7 @@ class CartaoPadrao extends StatelessWidget {
         color: cor,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: filhoCartao,
     );
   }
 }
